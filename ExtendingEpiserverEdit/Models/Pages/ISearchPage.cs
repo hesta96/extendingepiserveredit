416e464a6cbf9b5efdae2741af9ce79ac5594076
Henrik Fransas <henrik.fransas@netrelations.com>
2019-03-25 14:05:56 +0100

    Add project files.

---
+++ ExtendingEpiserverEdit/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace ExtendingEpiserverEdit.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
